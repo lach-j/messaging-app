1188e0a4b34c43de16942e3de66b79252a543277
Lachlan Johnson <lachlanm.johnson@gmail.com>
2021-12-05 10:12:48 +1100

    Remove websocket tests

--- messaging-frontend/src/App.js
+++ messaging-frontend/src/App.js
@@ -1,34 +1,7 @@
 import './App.css';
-import { io } from 'socket.io-client';
-import React, { useState, useEffect } from 'react';
-function App() {
-  const [socket, setSocket] = useState(null);
 
-  useEffect(() => {
-    const newSocket = io(`http://${window.location.hostname}:3000`);
-    console.log(newSocket);
-    setSocket(newSocket);
-    return () => newSocket.close();
-  }, [setSocket]);
-  return (
-    <div className="App">
-      <header className="app-header">React Chat</header>
-      {socket ? (
-        <>
-          <div className="chat-container">connected</div>
-          <button
-            onClick={(e) => {
-              console.log(socket.emit('custom_event', { payload: 'hello' }));
-            }}
-          >
-            SEND MSG
-          </button>
-        </>
-      ) : (
-        <div>Not Connected</div>
-      )}
-    </div>
-  );
+function App() {
+  return <div className="App"></div>;
 }
 
 export default App;
